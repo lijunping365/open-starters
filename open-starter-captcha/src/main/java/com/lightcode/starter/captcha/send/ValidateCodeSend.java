8266c35650364b56f195af137f285948ec7f7ffc
李俊平 <2544054976@qq.com>
2022-03-29 14:12:12 +0800

    fix:lambda表达式和泛型函数式接口的结合

--- open-starter-captcha/src/main/java/com/lightcode/starter/captcha/send/ValidateCodeSend.java
+++ open-starter-captcha/src/main/java/com/lightcode/starter/captcha/send/ValidateCodeSend.java
@@ -6,7 +6,7 @@ import com.lightcode.starter.captcha.core.sms.ValidateCode;
  * @author lijunping on 2022/3/25
  */
 @FunctionalInterface
-public interface ValidateCodeSend {
+public interface ValidateCodeSend<C extends ValidateCode> {
 
-    <C extends ValidateCode> void send(C validateCode);
+    void send(C validateCode);
 }
