ba4a0c348c31df7ff4e57153c59a88f76294269b
李俊平 <2544054976@qq.com>
2022-04-01 15:07:54 +0800

    feat:优化安全认证插件

--- open-starter-security/src/main/java/com/lightcode/starter/security/config/SecurityAutoConfiguration.java
+++ open-starter-security/src/main/java/com/lightcode/starter/security/config/SecurityAutoConfiguration.java
@@ -1,5 +1,6 @@
 package com.lightcode.starter.security.config;
 
+import com.lightcode.starter.security.interceptor.SecurityInterceptor;
 import com.lightcode.starter.security.properties.SecurityProperties;
 import com.lightcode.starter.security.service.TokenService;
 import com.lightcode.starter.security.service.support.JwtTokenService;
@@ -7,6 +8,9 @@ import org.springframework.boot.autoconfigure.condition.ConditionalOnMissingBean
 import org.springframework.boot.context.properties.EnableConfigurationProperties;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Import;
+import org.springframework.web.servlet.config.annotation.InterceptorRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 
 /**
@@ -15,6 +19,7 @@ import org.springframework.context.annotation.Configuration;
  */
 @Configuration
 @EnableConfigurationProperties(SecurityProperties.class)
+@Import(SecurityConfiguration.class)
 public class SecurityAutoConfiguration {
 
     @Bean
---
+++ open-starter-security/src/main/java/com/lightcode/starter/security/config/SecurityConfiguration.java
@@ -0,0 +1,27 @@
+package com.lightcode.starter.security.config;
+
+import com.lightcode.starter.security.interceptor.SecurityInterceptor;
+import com.lightcode.starter.security.properties.SecurityProperties;
+import org.springframework.web.servlet.config.annotation.InterceptorRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+/**
+ * @author lijunping on 2022/4/1
+ */
+public class SecurityConfiguration implements WebMvcConfigurer {
+
+    private final SecurityProperties securityProperties;
+    private final SecurityInterceptor securityInterceptor;
+
+    public SecurityConfiguration(SecurityProperties securityProperties, SecurityInterceptor securityInterceptor) {
+        this.securityProperties = securityProperties;
+        this.securityInterceptor = securityInterceptor;
+    }
+
+    @Override
+    public void addInterceptors(InterceptorRegistry registry) {
+        registry.addInterceptor(securityInterceptor)
+                .excludePathPatterns(securityProperties.getIgnorePaths())
+                .excludePathPatterns(securityProperties.getDefaultIgnorePaths());
+    }
+}
