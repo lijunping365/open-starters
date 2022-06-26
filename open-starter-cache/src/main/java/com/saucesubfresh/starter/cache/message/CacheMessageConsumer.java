686c081644276f27231abeec70da0a8a1d976e3a
lijunping <2544054976@qq.com>
2022-06-27 07:37:35 +0800

    feat：缓存组件缓存同步消息

--- open-starter-cache/src/main/java/com/saucesubfresh/starter/cache/message/CacheMessageConsumer.java
+++ open-starter-cache/src/main/java/com/saucesubfresh/starter/cache/message/CacheMessageConsumer.java
@@ -1,10 +1,23 @@
 package com.saucesubfresh.starter.cache.message;
 
 /**
+ * <b>
+ *     接受其他节点发送的广播消息，同步本地缓存
+ *
+ *     具体实现可参考 {@link RedissonCacheMessageConsumer,
+ *                  @link RedisCacheMessageConsumer,
+ *                  @link KafkaCacheMessageConsumer}
+ *
+ * </b>
  * @author: 李俊平
  * @Date: 2022-06-25 17:14
  */
 public interface CacheMessageConsumer {
 
+    /**
+     * 接受同步消息，同步本地缓存
+     *
+     * @param message 消息
+     */
     void onMessage(CacheMessage message);
 }
--- open-starter-cache/src/main/java/com/saucesubfresh/starter/cache/message/CacheMessageProducer.java
+++ open-starter-cache/src/main/java/com/saucesubfresh/starter/cache/message/CacheMessageProducer.java
@@ -1,10 +1,24 @@
 package com.saucesubfresh.starter.cache.message;
 
 /**
+ * <b>
+ *     发送消息给其他节点，广播模式
+ *
+ *     目的就是使其他节点的本地缓存同步
+ *
+ *     具体实现可参考 {@link RedissonCacheMessageProducer,
+ *                  @link RedisCacheMessageProducer,
+ *                  @link KafkaCacheMessageProducer}
+ * </b>
  * @author: 李俊平
  * @Date: 2022-06-25 17:14
  */
 public interface CacheMessageProducer {
 
+    /**
+     * 通知其他节点同步缓存
+     *
+     * @param message 消息
+     */
     void broadcastLocalCacheStore(CacheMessage message);
 }
