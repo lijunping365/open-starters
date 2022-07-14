c35a372cfeeae00509861533e115c747829c30af
李俊平 <2544054976@qq.com>
2022-07-14 15:44:08 +0800

    feat：爬虫组件

---
+++ open-starter-crawler/src/main/java/com/saucesubfresh/starter/crawler/pipeline/FilterPipeline.java
@@ -0,0 +1,10 @@
+package com.saucesubfresh.starter.crawler.pipeline;
+
+/**
+ * 过滤器
+ *
+ * @author lijunping on 2022/7/14
+ */
+public interface FilterPipeline extends Pipeline {
+
+}
---
+++ open-starter-crawler/src/main/java/com/saucesubfresh/starter/crawler/pipeline/SerializePipeline.java
@@ -0,0 +1,9 @@
+package com.saucesubfresh.starter.crawler.pipeline;
+
+/**
+ * 序列化
+ *
+ * @author lijunping on 2022/7/14
+ */
+public interface SerializePipeline extends Pipeline {
+}
