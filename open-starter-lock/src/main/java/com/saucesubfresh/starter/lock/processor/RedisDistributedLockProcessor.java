3fbd56ca65451a8cabf62f2df9102fa931b382d3
李俊平 <2544054976@qq.com>
2022-09-20 15:01:06 +0800

    :art: redis lock

--- open-starter-lock/src/main/java/com/saucesubfresh/starter/lock/processor/RedisDistributedLockProcessor.java
+++ open-starter-lock/src/main/java/com/saucesubfresh/starter/lock/processor/RedisDistributedLockProcessor.java
@@ -1,7 +1,11 @@
 package com.saucesubfresh.starter.lock.processor;
 
+import com.saucesubfresh.starter.lock.exception.LockException;
 import org.springframework.data.redis.core.RedisTemplate;
+import org.springframework.data.redis.core.script.RedisScript;
 
+import java.util.Collections;
+import java.util.List;
 import java.util.concurrent.TimeUnit;
 import java.util.function.Supplier;
 
@@ -10,20 +14,36 @@ import java.util.function.Supplier;
  */
 public class RedisDistributedLockProcessor implements DistributedLockProcessor{
 
+    private final RedisScript<List<Long>> script;
     private final RedisTemplate<String, Object> redisTemplate;
 
-    public RedisDistributedLockProcessor(RedisTemplate<String, Object> redisTemplate) {
+    public RedisDistributedLockProcessor(RedisScript<List<Long>> script,
+                                         RedisTemplate<String, Object> redisTemplate) {
+        this.script = script;
         this.redisTemplate = redisTemplate;
     }
 
-
     @Override
     public <T> T lock(Supplier<T> callback, String lockName, long leaseTime, TimeUnit timeUnit, boolean fairLock) throws Exception {
-        return null;
+        throw new LockException("Not support exception");
     }
 
     @Override
     public <T> T tryLock(Supplier<T> callback, String lockName, long waitTime, long leaseTime, TimeUnit timeUnit, boolean fairLock) throws Exception {
+        long threadId = Thread.currentThread().getId();
+        try {
+            Boolean acquired = redisTemplate.opsForValue().setIfAbsent(lockName, threadId, leaseTime, timeUnit);
+            if (Boolean.TRUE.equals(acquired)){
+                return callback.get();
+            }
+        }finally {
+            deleteLock(lockName, threadId);
+        }
         return null;
+    }
+
+
+    private void deleteLock(String lockName, long value){
+        this.redisTemplate.execute(this.script, Collections.singletonList(lockName), value);
     }
 }
