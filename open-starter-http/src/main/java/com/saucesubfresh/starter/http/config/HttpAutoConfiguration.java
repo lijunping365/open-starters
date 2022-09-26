eb5adccd0a228571866bb8407ec50b49dc49afbb
李俊平 <2544054976@qq.com>
2022-09-26 16:44:03 +0800

    :copyright: 版权

--- open-starter-http/src/main/java/com/saucesubfresh/starter/http/config/HttpAutoConfiguration.java
+++ open-starter-http/src/main/java/com/saucesubfresh/starter/http/config/HttpAutoConfiguration.java
@@ -1,3 +1,18 @@
+/*
+ * Copyright © 2022 organization SauceSubFresh
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
 package com.saucesubfresh.starter.http.config;
 
 import com.saucesubfresh.starter.http.constants.HttpConstant;
@@ -19,8 +34,7 @@ import org.springframework.context.annotation.Configuration;
 import java.util.concurrent.TimeUnit;
 
 /**
- * @author: 李俊平
- * @Date: 2022-01-26 09:10
+ * @author lijunping
  */
 @Configuration(proxyBeanMethods = false)
 @EnableConfigurationProperties(HttpProperties.class)
--- open-starter-http/src/main/java/com/saucesubfresh/starter/http/executor/support/DefaultHttpClientExecutor.java
+++ open-starter-http/src/main/java/com/saucesubfresh/starter/http/executor/support/DefaultHttpClientExecutor.java
@@ -1,3 +1,18 @@
+/*
+ * Copyright © 2022 organization SauceSubFresh
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
 package com.saucesubfresh.starter.http.executor.support;
 
 import com.saucesubfresh.starter.http.exception.HttpException;
@@ -23,6 +38,9 @@ import java.net.URISyntaxException;
 import java.nio.charset.Charset;
 import java.util.Map;
 
+/**
+ * @author lijunping
+ */
 @Slf4j
 public class DefaultHttpClientExecutor extends AbstractHttpExecutor implements HttpClientExecutor {
 
