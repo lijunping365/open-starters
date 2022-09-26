8c2edf0ec2fe302ff4907c96e297a779da68b831
李俊平 <2544054976@qq.com>
2022-09-26 16:37:47 +0800

    :copyright: 版权

--- open-starter-crawler/src/main/java/com/saucesubfresh/starter/crawler/pipeline/AbstractParserPipeline.java
+++ open-starter-crawler/src/main/java/com/saucesubfresh/starter/crawler/pipeline/AbstractParserPipeline.java
@@ -1,3 +1,18 @@
+/*
+ * Copyright © 2022 organization SauceSubFresh
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
 package com.saucesubfresh.starter.crawler.pipeline;
 
 import com.saucesubfresh.starter.crawler.domain.FieldExtractor;
@@ -15,11 +30,13 @@ import java.util.List;
 import java.util.Map;
 
 /**
- * 抽象解析类，子类实现包括
- * 1. 动态规则解析（自动模式）
- * 2. 基于类注解解析（手动模式）
- * @author: 李俊平
- * @Date: 2022-04-19 22:49
+ * <pre>
+ *  抽象解析类，子类实现包括
+ *  1. 动态规则解析（自动模式）
+ *  2. 基于类注解解析（手动模式）
+ * </pre>
+ *
+ * @author lijunping
  */
 public abstract class AbstractParserPipeline implements ParserPipeline {
 
--- open-starter-crawler/src/main/java/com/saucesubfresh/starter/crawler/pipeline/ParserPipeline.java
+++ open-starter-crawler/src/main/java/com/saucesubfresh/starter/crawler/pipeline/ParserPipeline.java
@@ -1,9 +1,24 @@
+/*
+ * Copyright © 2022 organization SauceSubFresh
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
 package com.saucesubfresh.starter.crawler.pipeline;
 
 /**
  * 数据解析
  *
- * @author lijunping on 2022/7/13
+ * @author lijunping
  */
 public interface ParserPipeline extends Pipeline{
 
