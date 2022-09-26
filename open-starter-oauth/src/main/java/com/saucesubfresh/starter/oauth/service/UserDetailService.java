a956a44e7da782f3950b0f7dd1c5ad67a455b4da
李俊平 <2544054976@qq.com>
2022-09-26 18:12:37 +0800

    :copyright: 版权

--- open-starter-oauth/src/main/java/com/saucesubfresh/starter/oauth/service/UserDetailService.java
+++ open-starter-oauth/src/main/java/com/saucesubfresh/starter/oauth/service/UserDetailService.java
@@ -1,12 +1,27 @@
+/*
+ * Copyright © 2022 organization SauceSubFresh
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
 package com.saucesubfresh.starter.oauth.service;
 
 
 import com.saucesubfresh.starter.oauth.domain.UserDetails;
 
 /**
- * @author : lijunping
- * @weixin : ilwq18242076871
- * Description: 用户信息接口
+ * 用户信息接口
+ *
+ * @author lijunping
  */
 public interface UserDetailService {
 
--- open-starter-oauth/src/main/java/com/saucesubfresh/starter/oauth/token/support/jwt/JwtTokenStore.java
+++ open-starter-oauth/src/main/java/com/saucesubfresh/starter/oauth/token/support/jwt/JwtTokenStore.java
@@ -1,3 +1,18 @@
+/*
+ * Copyright © 2022 organization SauceSubFresh
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
 package com.saucesubfresh.starter.oauth.token.support.jwt;
 
 
@@ -17,9 +32,9 @@ import lombok.extern.slf4j.Slf4j;
 import java.util.Date;
 
 /**
- * @author : lijunping
- * @weixin : ilwq18242076871
- * Description: Jwt token store，把 token 加密传给客户端
+ * Jwt token store，把生成的 jwt token 传给客户端
+ *
+ * @author lijunping
  */
 @Slf4j
 public class JwtTokenStore extends AbstractTokenStore {
