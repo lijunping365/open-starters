df2b56ea8bc2479383b7cdfd0e8f9e5a854805a2
lijunping <2544054976@qq.com>
2023-01-29 22:40:35 +0800

    :art: 优化缓存

--- open-starter-cache/src/main/java/com/saucesubfresh/starter/cache/properties/CacheProperties.java
+++ open-starter-cache/src/main/java/com/saucesubfresh/starter/cache/properties/CacheProperties.java
@@ -18,8 +18,6 @@ package com.saucesubfresh.starter.cache.properties;
 import lombok.Data;
 import org.springframework.boot.context.properties.ConfigurationProperties;
 
-import java.util.concurrent.ThreadLocalRandom;
-
 /**
  * @author lijunping
  */
